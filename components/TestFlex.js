1c8095e471b781e73833fba3362bc0fa8038dc2c
ESS Systemes & Services <lyrbat@gmail.com>
2020-03-28 13:25:17 +0100

    commit du samedi

--- components/TestFlex.js
+++ components/TestFlex.js
@@ -6,9 +6,10 @@ export default class TestFlex extends Component {
     return (
       // Try setting `flexDirection` to `column`.
       <View style={{flex: 1, flexDirection: 'row'}}>
-        <View style={{flex:1, backgroundColor: '#8b6550'}} />
+        <View style={{flex:2, backgroundColor: '#8b6550'}} />
         <View style={{flex:1, backgroundColor: '#85573e'}} />
         <View style={{flex:1, backgroundColor: '#96512b'}} />
+        <View style={{backgroundColor: '#96612b'}} />
       </View>
     );
   }
